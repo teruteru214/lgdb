2fda7d8a519a5bda10594e43dc33ed57c78fcea9
teruteru <tutituti3080@gmail.com>
2023-05-23 22:03:14 +0900

    first commit

---
+++ src/components/GameSearch.tsx
@@ -0,0 +1,55 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+interface Game {
+  id: number;
+  name: string;
+  cover: {
+    id: number;
+    url: string;
+  };
+}
+
+const GameSearch: React.FC = () => {
+  const [searchQuery, setSearchQuery] = useState("");
+  const [gameCovers, setGameCovers] = useState<Game[]>([]);
+
+  const handleSearch = async () => {
+    try {
+      const response = await axios.post<Game[]>(
+        "https://api.igdb.com/v4/games",
+        `fields id, name, cover.url; limit 28; search "${searchQuery}";`,
+        {
+          headers: {
+            "Client-ID": "8l6p4sfo8pyuwpoc2ki4ncbbjyrcw2",
+            Authorization: "Bearer hyfx0bh9p03ts1vo06hslw7xuptok1",
+          },
+        }
+      );
+      setGameCovers(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <div>
+      <input
+        type="text"
+        value={searchQuery}
+        onChange={(e) => setSearchQuery(e.target.value)}
+      />
+      <button onClick={handleSearch}>Search</button>
+      <div>
+        {gameCovers.map((game) => (
+          <div key={game.id}>
+            <h3>{game.name}</h3>
+            <img src={game.cover.url} alt="Game Cover" />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default GameSearch;
